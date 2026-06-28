1bdecd3b95ca5254742781a5c161cfd74b9956b0
shubham <shubhamagrahari141@gmail.com>
2026-06-28 22:59:31 +0530

    docs: add execution screenshots

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,5 +1,5 @@
-const { Before, After } = require('@cucumber/cucumber');
-const { chromium } = require('playwright');
+const { Before, After, Status } = require("@cucumber/cucumber");
+const { chromium } = require("playwright");
 
 Before(async function () {
     this.browser = await chromium.launch({
@@ -7,10 +7,20 @@ Before(async function () {
     });
 
     this.context = await this.browser.newContext();
-
     this.page = await this.context.newPage();
 });
 
-After(async function () {
+After(async function (scenario) {
+
+    // Screenshot only when scenario passes
+    if (scenario.result.status === Status.PASSED) {
+
+        await this.page.screenshot({
+            path: `screenshots/${scenario.pickle.name}.png`,
+            fullPage: true
+        });
+
+    }
+
     await this.browser.close();
 });
